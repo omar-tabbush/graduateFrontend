f729d2b5b821d56f0a3103c0d760673198acf9b7
omar tabbush (personal laptop) <omartabbush02@gmail.com>
2023-05-03 11:37:20 +0300

    fixing env

--- src/api/axios.js
+++ src/api/axios.js
@@ -13,23 +13,7 @@ export const privateAxios = axios.create({
     "Content-Type": "application/json",
     authorization: `Bearer ${localStorage.getItem("token") || ""}`,
   },
-  //   transformRequest: [
-  //     (data) => {
-  //       return JSON.stringify({
-  //         ...{ storeId: +import.meta.env.VITE_STORE_ID },
-  //         ...data,
-  //       });
-  //     },
-  //     ...axios.defaults.transformRequest,
-  //   ],
+
 });
 
-export const imageApi = axios.create({
-  baseURL: "https://api.imgbb.com/1/",
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
-  params: {
-    key: "73d87ec05960039ef2e450c3ace2287b",
-  },
-});
+
